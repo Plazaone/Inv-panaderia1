1dc1e06e501d0687ef60732444c79bf51485576b
11102Jamd <jalejandromd17@gmail.com>
2025-02-20 22:42:01 -0500

    Cambio de Prueba Pull Request

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -20,6 +20,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->string('Telefono',12);
             $table->string('Direccion',50);
+            $table->string('Rol');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
